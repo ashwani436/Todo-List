06cdd0baa5b37848f2da3696432c0b9a7d697030
ashwani436 <sharmagpl786@gmail.com>
2021-03-18 13:17:59 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 const App=()=>{
+  
+const[inputList,setInputList]=useState("");
+const[Items,setItem]=useState([]);
+
+
+
+const itemEvent=(event)=>{
+      setInputList(event.target.value);
+}
+const listofitem=()=>{
+        setItem((oldItems)=>{
+                   return [...oldItems,inputList];
+
+        })
+};
   return (
-    <div className="main-div">
-      <div className="center-div">
+    <div className="main_div">
+      <div className="center_div">
         <h1>Todo List</h1>
-        <input type='text' placeholder="Add a item"/>
-        <button> + </button>
+        <input type='text' placeholder="Add a item" onChange={itemEvent}/>
+        <button onClick={listofitem}> + </button>
+
+        <ol>
+          {/*<li>{inputList}</li>*/}
+          {Items.map((value)=>{
+              return <li>{value}</li>;
+          })}
+        </ol>
       </div>
       
     </div>
